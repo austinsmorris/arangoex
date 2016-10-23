490362102f4e14dc1c55ada8554032d5e4aba3d5
Austin Morris <austin.morris@gmail.com>
2016-10-23 14:37:41 -0400

    setup package and dependencies

--- mix.exs
+++ mix.exs
@@ -2,19 +2,24 @@ defmodule Arangoex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :arangoex,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :arangoex,
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      name: "Arangoex",
+      deps: deps(),
+      description: description(),
+      package: package(),
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:httpoison, :logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +32,27 @@ defmodule Arangoex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:httpoison, "~> 0.9.0"},
+      {:poison, "~> 3.0"},
+    ]
+  end
+
+  defp description do
+    """
+    An Elixir driver for ArangoDB.
+    """
+  end
+
+  defp package do
+    [
+      files: ["config", "lib", "test", ".gitignore", "LICENSE*", "mix.exs", "README*"],
+      maintainers: ["Austin S. Morris"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/austinsmorris/arangoex"},
+    ]
   end
 end
+
+
