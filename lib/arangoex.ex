fa0f7236432600717530aad53e10879b254e88f3
Austin Morris <austin.morris@gmail.com>
2016-10-27 16:26:57 -0400

    udpate Arangoex.Response to handle conversions from HTTPoison

--- lib/arangoex.ex
+++ lib/arangoex.ex
@@ -1,2 +1,42 @@
 defmodule Arangoex do
+  @moduledoc false
+
+  alias Arangoex.Response
+
+  use HTTPoison.Base
+
+  @default_opts [host: "http://localhost:8529", database: "_system", username: "root", password: ""]
+
+  @base_url Application.get_env(:arangoex, :host, Keyword.fetch!(@default_opts, :host)) <>
+    "/_db/" <> Application.get_env(:arangoex, :database, Keyword.fetch!(@default_opts, :database))
+
+  @authorization "Basic " <> Base.encode64(
+    Application.get_env(:arangoex, :username, Keyword.fetch!(@default_opts, :username)) <> ":" <>
+    Application.get_env(:arangoex, :password, Keyword.fetch!(@default_opts, :password))
+  )
+
+  def add_base_url(url, opts) do
+    @base_url <> url
+  end
+
+  def get(url, headers \\ [], options \\ []) do
+    request(:get, url, "", get_headers(headers), options) |> Response.convert_response()
+  end
+
+  def head(url, headers \\ [], options \\ []) do
+    request(:head, url, "", get_headers(headers), options) |> Response.convert_response()
+  end
+
+  def post(url, body, headers \\ [], options \\ []) do
+    request(:post, url, body, get_headers(headers), options) |> Response.convert_response()
+  end
+
+  def put(url, body \\ "", headers \\ [], options \\ []) do
+    request(:put, url, body, get_headers(headers), options) |> Response.convert_response()
+  end
+
+  defp get_headers(headers) do
+    headers = headers ++ [{"Accept", "application/json"}, {"Authorization", "Basic " <> @authorization}]
+    Enum.dedup_by(headers, fn({header, _value}) -> header end)
+  end
 end
--- test/arangoex/response_test.exs
+++ test/arangoex/response_test.exs
@@ -2,11 +2,28 @@ defmodule Arangoex.ResponseTest do
   use ExUnit.Case
 
   alias Arangoex.Response
+  alias HTTPoison.Response, as: HTTPoisonResponse
 
   test "Response struct" do
     response = %Response{}
     assert response.body == nil
     assert response.headers == []
     assert response.status_code == nil
+  end
+
+  test "convert HTTPoison.Response" do
+    response = %HTTPoisonResponse{body: "foo", headers: [{"foo", "bar"}], status_code: 123}
+    converted = Response.convert_response(response)
+    assert converted.body == "foo"
+    assert converted.headers == [{"foo", "bar"}]
+    assert converted.status_code == 123
+  end
+
+  test "convert HTTPoison.Response tuple" do
+    response = %HTTPoisonResponse{body: "foo", headers: [{"foo", "bar"}], status_code: 123}
+    {:ok, converted} = Response.convert_response({:ok, response})
+    assert converted.body == "foo"
+    assert converted.headers == [{"foo", "bar"}]
+    assert converted.status_code == 123
   end
 end
