e95b8c1abab490380024591c11530d9af567d474
Austin Morris <austin.morris@gmail.com>
2016-11-16 11:36:22 -0500

    initial implementation for transaction execution

--- lib/arangoex/transaction.ex
+++ lib/arangoex/transaction.ex
@@ -1,6 +1,18 @@
 defmodule Arangoex.Transaction do
   @moduledoc false
 
-  # POST /_api/transaction
+  alias Arangoex.JSON
 
+  @base_url "/_api/transaction"
+
+  # POST /_api/transaction
+  # Execute a transaction.
+  def execute(%{} = transaction) do
+    {:ok, body} = JSON.encode(transaction)
+    [] |> build_url() |> Arangoex.post(body)
+  end
+
+  defp build_url(url_part) do
+    [Arangoex.add_base_url(@base_url), "/", url_part]
+  end
 end
