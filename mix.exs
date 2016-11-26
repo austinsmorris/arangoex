6525ebb721301eca116f4db9c90ac27db09b876e
Austin Morris <austin.morris@gmail.com>
2016-11-26 13:51:03 -0500

    roll 0.0.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Arangoex.Mixfile do
   def project do
     [
       app: :arangoex,
-      version: "0.0.7",
+      version: "0.0.8",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
