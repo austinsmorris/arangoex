32d419014ff8577cb1cc4427443150c8e33528f1
Austin Morris <austin.morris@gmail.com>
2017-07-02 20:18:56 -0400

    implement Arangoex.User

--- lib/arangoex/user.ex
+++ lib/arangoex/user.ex
@@ -1,13 +1,212 @@
 defmodule Arangoex.User do
-  @moduledoc false
+  @moduledoc """
+  This module contains functions used to manage users.
+  """
 
-  # POST /_api/user
-  # PUT /_api/user/{user}/database/{dbname}
-  # GET /_api/user/{user}/database
-  # PUT /_api/user/{user}
-  # PATCH /_api/user/{user}
-  # DELETE /_api/user/{user}
-  # GET /_api/user/{user}
-  # GET /_api/user
+  @doc """
+  Create a new user.
 
+  The `conn` parameter is an ArangoDB connection PID.  The `user` parameter is a map describing the user to be created.
+
+  ## Endpoint
+
+  POST /_api/user
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+      {:ok, conn} = Arangoex.start_link()
+      {:ok, resp} = Arangoex.User.create(conn, %{user: "foo"})
+  """
+  def create(conn, %{} = user, opts \\ []) do
+    Arangoex.request(conn, :post, "/_api/user", %{}, %{}, user, opts)
+  end
+
+  @doc """
+  Return information about the databases available to a user.
+
+  The `conn` parameter is an ArangoDB connection PID.  The `user_name` parameter is a string name of the user whose
+  database information should be returned.
+
+  ## Endpoint
+
+   GET /_api/user/{user_name}/database
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+     {:ok, conn} = Arangoex.start_link()
+     {:ok, resp} = Arangoex.User.database(conn, "foo")
+  """
+  def database(conn, user_name, opts \\ []) do
+    Arangoex.request(conn, :get, "/_api/user/#{user_name}/database", %{}, %{}, nil, opts)
+  end
+
+  @doc """
+  Return information about a user.
+
+  The `conn` parameter is an ArangoDB connection PID.  The `user_name` parameter is a string name of the user whose
+  information should be returned.
+
+  ## Endpoint
+
+   GET /_api/user/{user_name}
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+     {:ok, conn} = Arangoex.start_link()
+     {:ok, resp} = Arangoex.User.get(conn, "foo")
+  """
+  def get(conn, user_name, opts \\ []) do
+    Arangoex.request(conn, :get, "/_api/user/#{user_name}", %{}, %{}, nil, opts)
+  end
+
+  @doc """
+  Grant database access to a user.
+
+  The `conn` parameter is an ArangoDB connection PID.  The `user_name` parameter is a string name of the user who will
+  be granted database access.  The `database_name` parameter is the string name of the database to which the user will
+  be granted access.
+
+  ## Endpoint
+
+  PUT /_api/user/{user}/database/{dbname}
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+      {:ok, conn} = Arangoex.start_link()
+      {:ok, resp} = Arangoex.User.grant(conn, "foo", "bar")
+  """
+  def grant(conn, user_name, database_name, opts \\ []) do
+    Arangoex.request(conn, :put, "/_api/user/#{user_name}/database/#{database_name}", %{}, %{}, %{grant: "rw"}, opts)
+  end
+
+  @doc """
+  Return information about all users the current user has access to.
+
+  The `conn` parameter is an ArangoDB connection PID.
+
+  ## Endpoint
+
+  GET /_api/user
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+      {:ok, conn} = Arangoex.start_link()
+      {:ok, resp} = Arangoex.User.list(conn)
+  """
+  def list(conn, opts \\ []) do
+    Arangoex.request(conn, :get, "/_api/user", %{}, %{}, nil, opts)
+  end
+
+  @doc """
+  Remove a user from the system.
+
+  The `conn` parameter is an ArangoDB connection PID.  The `user_name` parameter is a string name of the user to be
+  removed.
+
+  ## Endpoint
+
+  DELETE /_api/user/{user_name}
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+      {:ok, conn} = Arangoex.start_link()
+      {:ok, resp} = Arangoex.User.remove(conn, "foo")
+  """
+  def remove(conn, user_name, opts \\ []) do
+    Arangoex.request(conn, :delete, "/_api/user/#{user_name}", %{}, %{}, nil, opts)
+  end
+
+  @doc """
+  Replace the properties of a user.
+
+  The `conn` parameter is an ArangoDB connection PID.  The `user_name` parameter is the string name of the user whose
+  properties should be replaced.  The `user` parameter is a map describing the replacement user properties.
+
+  ## Endpoint
+
+  PUT /_api/user/{user_name}
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+      {:ok, conn} = Arangoex.start_link()
+      {:ok, resp} = Arangoex.User.replace(conn, "foo", %{password: "bar")
+  """
+  def replace(conn, user_name, %{} = user, opts \\ []) do
+    Arangoex.request(conn, :put, "/_api/user/#{user_name}", %{}, %{}, user, opts)
+  end
+
+  @doc """
+  Revoke database access from a user.
+
+  The `conn` parameter is an ArangoDB connection PID.  The `user_name` parameter is a string name of the user whose
+  access will be revoked. The `database_name` parameter is the string name of the database to which the user's access
+  will be revoked.
+
+  ## Endpoint
+
+  PUT /_api/user/{user}/database/{dbname}
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+      {:ok, conn} = Arangoex.start_link()
+      {:ok, resp} = Arangoex.User.revoke(conn, "foo", "bar")
+  """
+  def revoke(conn, user_name, database_name, opts \\ []) do
+    Arangoex.request(conn, :put, "/_api/user/#{user_name}/database/#{database_name}", %{}, %{}, %{grant: "none"}, opts)
+  end
+
+  @doc """
+  Update the properties of a user.
+
+  The `conn` parameter is an ArangoDB connection PID.  The `user_name` parameter is the string name of the user whose
+  properties should be updated.  The `user` parameter is a map describing the updated user properties.
+
+  ## Endpoint
+
+  PATCH /_api/user/{user_name}
+
+  ## Options
+
+  See the "Shared Options" in the `Arangoex` module documentation for additional options.
+
+  ## Examples
+
+      {:ok, conn} = Arangoex.start_link()
+      {:ok, resp} = Arangoex.User.update(conn, "foo", %{password: "bar")
+  """
+  def update(conn, user_name, %{} = user, opts \\ []) do
+    Arangoex.request(conn, :patch, "/_api/user/#{user_name}", %{}, %{}, user, opts)
+  end
 end
--- test/arangoex/user_test.exs
+++ test/arangoex/user_test.exs
@@ -1,7 +1,148 @@
 defmodule Arangoex.UserTest do
   alias Arangoex.User
 
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
-  # todo - User tests
+  setup do
+    # make sure "foo" user does not exist for tests
+    User.remove(:arango, "foo")
+
+    on_exit fn ->
+      # make sure "foo" is removed from any test
+      User.remove(:arango, "foo")
+    end
+  end
+
+  test "create() creates a new user." do
+    {:ok, resp} = User.create(:arango, %{user: "foo"})
+
+    assert resp.status_code === 201
+    assert resp.body["code"] === 201
+    assert resp.body["error"] === false
+    assert resp.body["active"] === true
+    assert resp.body["changePassword"] === false
+    assert resp.body["extra"] === %{}
+    assert resp.body["user"] === "foo"
+
+    foo = %{"active" => true, "changePassword" => false, "extra" => %{}, "user" => "foo"}
+    {:ok, resp} = User.list(:arango)
+
+    assert Enum.member?(resp.body["result"], foo)
+  end
+
+  test "database() shows information about databases a user has access to." do
+    User.create(:arango, %{user: "foo"})
+    User.grant(:arango, "foo", "test")
+    {:ok, resp} = Arangoex.User.database(:arango, "foo")
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+    assert resp.body["result"] === %{"test" => "rw"}
+  end
+
+  test "get() show information about a user." do
+    User.create(:arango, %{user: "foo"})
+    {:ok, resp} = User.get(:arango, "foo")
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+    assert resp.body["active"] === true
+    assert resp.body["changePassword"] === false
+    assert resp.body["extra"] === %{}
+    assert resp.body["user"] === "foo"
+  end
+
+  test "grant() grants database access to a user." do
+    User.create(:arango, %{user: "foo"})
+    User.grant(:arango, "foo", "test")
+    {:ok, resp} = Arangoex.User.database(:arango, "foo")
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+    assert resp.body["result"] === %{"test" => "rw"}
+  end
+
+  test "list() shows users in the system." do
+    User.create(:arango, %{user: "foo"})
+    {:ok, resp} = User.list(:arango)
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+
+    foo = %{"active" => true, "changePassword" => false, "extra" => %{}, "user" => "foo"}
+    assert Enum.member?(resp.body["result"], foo)
+  end
+
+  test "remove() removes a user from the system." do
+    User.create(:arango, %{user: "foo"})
+    {:ok, resp} = User.remove(:arango, "foo")
+
+    assert resp.status_code === 202
+    assert resp.body["code"] === 202
+    assert resp.body["error"] === false
+
+    {:ok, resp} = User.list(:arango)
+
+    foo = %{"active" => true, "changePassword" => false, "extra" => %{}, "user" => "foo"}
+    refute Enum.member?(resp.body["result"], foo)
+  end
+
+  test "replace() replaces the properties of a user." do
+    User.create(:arango, %{user: "foo"})
+    {:ok, resp} = User.replace(:arango, "foo", %{active: false})
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+    assert resp.body["active"] === false
+    assert resp.body["changePassword"] === false
+    assert resp.body["extra"] === %{}
+    assert resp.body["user"] === "foo"
+
+    {:ok, resp} = User.list(:arango)
+
+    foo = %{"active" => false, "changePassword" => false, "extra" => %{}, "user" => "foo"}
+    assert Enum.member?(resp.body["result"], foo)
+  end
+
+  test "revoke() revokes database access from a user." do
+    User.create(:arango, %{user: "foo"})
+    User.grant(:arango, "foo", "test")
+    {:ok, resp} = Arangoex.User.database(:arango, "foo")
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+    assert resp.body["result"] === %{"test" => "rw"}
+
+    User.revoke(:arango, "foo", "test")
+    {:ok, resp} = Arangoex.User.database(:arango, "foo")
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+    assert resp.body["result"] === %{"test" => "none"}
+  end
+
+  test "update() updates the properties of a user." do
+    User.create(:arango, %{user: "foo"})
+    {:ok, resp} = User.replace(:arango, "foo", %{active: false})
+
+    assert resp.status_code === 200
+    assert resp.body["code"] === 200
+    assert resp.body["error"] === false
+    assert resp.body["active"] === false
+    assert resp.body["changePassword"] === false
+    assert resp.body["extra"] === %{}
+    assert resp.body["user"] === "foo"
+
+    {:ok, resp} = User.list(:arango)
+
+    foo = %{"active" => false, "changePassword" => false, "extra" => %{}, "user" => "foo"}
+    assert Enum.member?(resp.body["result"], foo)
+  end
 end
